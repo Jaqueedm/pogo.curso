15e6e11ecb8a8f317936f1c0acf91815909885a6
Jaqueedm <dianithamayo@gmail.com>
2021-07-02 16:01:49 -0500

    Update show.blade.php

--- resources/views/Cursos/show.blade.php
+++ resources/views/Cursos/show.blade.php
@@ -7,9 +7,12 @@
 <div class="card" style="width: 18rem;">
     <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Bienvenido  {{$curso->name}}</h5>
-      <p class="card-text"><strong>Categoria:</strong>{{$curso->categoria}}</p>
+      <h5 class="card-title"> {{$curso->name}}</h5>
+      <h1 class="card-text">{{$curso->genero}}</h1>
+      <p class="card-text"><strong>Tipo:</strong>{{$curso->tipo_poke}}</p>
       <p class="card-text">{{$curso->descripcion}}</p>
+      <p class="card-text"><strong>Región </strong>{{$curso->region}}</p>
+
       <a href="{{route('cursos.edit', $curso)}}" class="btn btn-primary">Editar pogo</a>
       <form action="{{route('cursos.destroy', $curso)}}" method="POST">
         @csrf
